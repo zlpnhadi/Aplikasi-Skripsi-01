1649d4b0b32283bb36a0183a3dc62592afbda485
zlpnhadi <107482139+zlpnhadi@users.noreply.github.com>
2024-03-09 22:50:16 +0700

    first commit

---
+++ config/databases.php
@@ -0,0 +1,8 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "siapproposal";
+// Create connection
+$con = mysqli_connect($servername, $username, $password, $dbname);
